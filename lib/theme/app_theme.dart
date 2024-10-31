d68d329516583f078cea4b6530a5c8dca5d28765
Pratik-2904 <pratiksukale2904@gmail.com>
2024-10-31 19:53:42 +0530

    Addtion of single file to manage the theme throughout the appplication

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,8 +1,6 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-class AppColors {
+class AppTheme {
   static const Color primaryColor = Color(0xFF2C3A3A); // Dark teal
   static const Color secondaryColor = Color(0xFF243232); // Darker teal
   static const Color cardColor = Color(0xFF1E2828); // Darkest teal
@@ -11,4 +9,9 @@ class AppColors {
   static const Color textColor = Colors.white; // White text
   static const Color subtleText = Colors.white60; // Subtle white
   static const Color fieldBg = Color(0xFF2F3D3D); // Light teal
+
+  static const Color scaffoldBackgroundColor = Colors.white;
+  static const Color accountInfoOverlayBackgroundColor = Color(0xFF212f31);
+  static const Color appBarBackgroundColor = Color(0xFF212f31);
+  static const Color accountInfoOverlaySecBackgroundColor = Color(0xFF344148);
 }
